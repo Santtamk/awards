a133b8dbda9e86fe7e88188cf7a1a4b7df572195
Santam Kumai <santtamk@gmail.com>
2024-12-26 15:16:42 +0530

    completed for now buy visual changes can be made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -72,7 +72,11 @@ const NavBar = () => {
         <nav className="flex size-full items-center justify-between p-4">
           {/* Logo and Product button */}
           <div className="flex items-center gap-7">
-            <img src="/img/logo.png" alt="logo" className="w-10" />
+            <img
+              src="https://avatars.githubusercontent.com/u/106295872?v=4"
+              alt="github_logo"
+              className="w-10"
+            />
 
             <Button
               id="product-button"
